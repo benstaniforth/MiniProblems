b4a40ddbc835b581edda126c47f957eef5fb859b
Ben Staniforth <benstaniforth@Bens-MacBook-Pro.local>
2019-03-01 11:34:58 +0000

    Summing up 3 and 5 modulos

--- src/benstaniforth/MiniProblems/SummingUp.java
+++ src/benstaniforth/MiniProblems/SummingUp.java
@@ -1,8 +1,30 @@
 package benstaniforth.MiniProblems;
 
+import java.util.Scanner;
+
 public class SummingUp {
 
     public static void main(String[] args) {
+
+        System.out.println("Welcome to \"Summing up\"");
+        System.out.print("Please enter a number > ");
+
+        Scanner scanner = new Scanner(System.in);
+        int numberSelection = scanner.nextInt();
+
+
+        int sum = 0;
+        for (int i = 0; i <= numberSelection; i++) {
+
+
+            if ((i % 3 == 0) || (i % 5 == 0)) {
+                sum += i;
+            }
+
+        }
+
+        System.out.println("The sum of all numbers is: " + sum);
+
 
     }
 }
