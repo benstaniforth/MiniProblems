de867e4b4bc2247aebf24bea22780c350d986329
Ben Staniforth <benstaniforth@Bens-MacBook-Pro.local>
2019-03-01 10:38:38 +0000

    First set of mini problems

---
+++ src/benstaniforth/MiniProblems/Main.java
@@ -0,0 +1,48 @@
+package benstaniforth.MiniProblems;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hi there, please enter your name");
+
+
+        Scanner scanner = new Scanner(System.in);
+        String personsName = scanner.nextLine();
+
+
+        if (personsName.equals("Alice")){
+            System.out.println("Hi there Alice");
+        }
+        else if (personsName.equals("Bob")){
+            System.out.println("Howdy Bob");
+        }
+        else {
+            System.out.println("Sorry, I don't know you");
+        }
+
+
+
+
+
+
+//        public static class promptForName {
+//
+//            Scanner name = new Scanner(System.in);
+//            String personsName = name.nextLine();
+//
+//
+//        }
+
+
+
+
+    }
+
+
+
+}
---
+++ src/benstaniforth/MiniProblems/SummingUp.java
@@ -0,0 +1,8 @@
+package benstaniforth.MiniProblems;
+
+public class SummingUp {
+
+    public static void main(String[] args) {
+
+    }
+}
